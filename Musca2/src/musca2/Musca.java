f4fbed52309ca8c2298315656bed6eaf3b157e82
cretzuwashere <opreacristianalexandru@gmail.com>
2014-07-10 13:45:13 +0300

    Versiunea 2.1.2
    Am mutat metodele ce aveau legatura cu orientarea/pozitia/poza din clasele muscavie/muscamoarta in clasa parinte musca

--- Musca2/src/musca2/Musca.java
+++ Musca2/src/musca2/Musca.java
@@ -5,13 +5,34 @@ import java.awt.Point;
 
 public class Musca {
 
-    private Image _musca;
-    private Point _pozitie;
+    protected Image _musca;
+    protected Point _pozitie;
+    protected Image _pozaMusca;
+    public Orientare _orientare;
 
-    public Musca(Image x,Point z) {
+    public Musca(Image x, Point z) {
         _musca = x;
         _pozitie = z;
     }
     ;
+    
+    public Point getPozitie() {
+        return _pozitie;
+    }
 
+    public void setPozitie(Point x) {
+        _pozitie = x;
+    }
+
+    public Image getPoza() {
+        return _pozaMusca;
+    }
+
+    public Orientare getOrientare() {
+        return _orientare;
+    }
+
+    public void setOrientare(Orientare x) {
+        _orientare = x;
+    }
 }
